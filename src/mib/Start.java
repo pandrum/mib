2a2ce0e8abe29d205a134f573e360623ded2f05a
carllindstrom <lindstrommcarl@gmail.com>
2020-01-07 12:08:27 +0100

    första commit

--- src/mib/Start.java
+++ src/mib/Start.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package mib;
+
 import oru.inf.*;
 import javax.swing.JOptionPane;
 
@@ -12,20 +13,19 @@ import javax.swing.JOptionPane;
  * @author sbf
  */
 public class Start {
-    
+
     private static InfDB db;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-           try {
-            db = new InfDB("/Users/Shared/MIB/db/MIBDB.FDB");
+        try {
+            db = new InfDB("/Users/Shared/db/MIBDB.FDB");
             new MainWindow(db).setVisible(true);
-        }
-        catch (InfException e) {
+        } catch (InfException e) {
             JOptionPane.showMessageDialog(null, e);
         }
     }
-    
+
 }

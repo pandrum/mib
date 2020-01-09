599d7635f69b99726aef2b975ce738b88ea07e6e
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2020-01-09 22:16:49 +0100

    lil pleb

--- src/mib/Start.java
+++ src/mib/Start.java
@@ -5,46 +5,27 @@
  */
 package mib;
 
-import java.io.File;
 import oru.inf.*;
 import javax.swing.JOptionPane;
 
 /**
+ *
  * @author sbf
  */
 public class Start {
 
-    private static InfDB idb;
+    private static InfDB db;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         try {
-            String dir = System.getProperty("user.dir");
-            String os = System.getProperty("os.name");
-            String dbPath = "";
-            switch (os) {
-                case "Mac OS X":
-                    System.out.println(os);
-                    dbPath = dir + "/db/MIBDB.FDB";
-                    File file = new File(dbPath);
-                    file.canExecute();
-                    file.canWrite();
-                    file.canRead();
-                    System.out.println(dbPath);
-                    break;
-                case "Windows 10":
-                    System.out.println(os);
-                    dbPath = dir + ("\\db\\MIBDB.FDB");
-                case "Windows 7":
-                    System.out.println(os);
-            }
-            idb = new InfDB(dbPath);
-            new MainWindow(idb).setVisible(true);
-
+            db = new InfDB("/Users/Shared/db/MIBDB.FDB");
+            new MainWindow(db).setVisible(true);
         } catch (InfException e) {
-            JOptionPane.showMessageDialog(null, "Ett fel inträffade!");
+            JOptionPane.showMessageDialog(null, e);
         }
     }
+
 }

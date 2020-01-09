360e6759a8ea8f956b385bd405f55e3b818d685f
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2020-01-09 21:52:21 +0100

    dafuq bruh

--- src/mib/Start.java
+++ src/mib/Start.java
@@ -9,23 +9,34 @@ import oru.inf.*;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author sbf
  */
 public class Start {
 
-    private static InfDB db;
+    private static InfDB idb;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         try {
-            db = new InfDB("/Users/Shared/db/MIBDB.FDB");
-            new MainWindow(db).setVisible(true);
+            String dir = System.getProperty("user.dir");
+            String os = System.getProperty("os.name");
+            String dbPath = "";
+            switch (os) {
+                case "Mac OS X":
+                    System.out.println(os);
+                    dbPath = dir + "/db/MIBDB.FDB";
+                    break;
+                case "Windows":
+                    System.out.println(os);
+                    dbPath = dir + ("\\db\\MIBDF.FDB");
+            }
+            idb = new InfDB(dbPath);
+            new MainWindow(idb).setVisible(true);
+
         } catch (InfException e) {
-            JOptionPane.showMessageDialog(null, e);
+            JOptionPane.showMessageDialog(null, "Ett fel inträffade!");
         }
     }
-
 }
